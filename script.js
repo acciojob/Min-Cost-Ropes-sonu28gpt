45d1ad773cd9a49dc11889072cae6be0b7c29c1f
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-11 23:37:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function  mini2(arr) {
 	let n=arr.length;
 	for(let i=0;i<2;i++){
-		for(let j=0;j<arr.length-2;j++){
+		for(let j=0;j<arr.length-1;j++){
 			if(arr[j]<arr[j+1]){
 				let temp=arr[j+1];
 				arr[j+1]=arr[j];
